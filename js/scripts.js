82dc34cbd47a903e997ae8da8a906d4c8c8e37a2
Erin Egbert <erinegbert6@gmail.com>
2021-07-28 12:00:07 -0700

    added the list of Pokemon and their details

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
-alert('Hello World');
-
-let favoriteFood = 'donuts';
-document.write(favoriteFood);
+// List of Pokemons and their name, height, and types
+let pokemonList = [
+	{
+		name: 'Bulbasaur',
+		height: .7,
+		type: [ 'poison', 'grass']
+	},
+	{
+		name: 'Squirtle',
+		height: .5,
+		type: [ 'water', 'defense']
+	},
+	{
+		name: 'Dragonite',
+		height: 2.2,
+		type: [ 'dragon', 'flying']
+	}
+];
