85bc222a6d3db66cf54f28027e8b03472981a435
Erin Egbert <erinegbert6@gmail.com>
2021-07-30 13:57:31 -0700

    update forEach loop to work with IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -34,6 +34,7 @@ let pokemonRepository = (function () {
 
 
 // forEach() loop to display pokemonList
+pokemonRepository.getAll().forEach( function (pokemon) {
 	if (pokemon.height > 1) {
 		document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\'s big! </p>');
 	} else {
